5dc0cb1dc4e33bc3f4c407265689c661987dfcb1
MaksymilianMikolajczakCode <maksymilianmikolajczakcode@gmail.com>
2023-07-12 03:12:08 +0100

    first commit

---
+++ components/Button.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({title}: {title: string}) => {
+  return (
+    <div>Button</div>
+  )
+}
+
+export default Button
---
+++ components/ProfileMenu.tsx
@@ -0,0 +1,10 @@
+import { SessionInterface } from '@/common.types'
+import React from 'react'
+
+const ProfileMenu = ({session}: {session: SessionInterface}) => {
+  return (
+    <div>ProfileMenu</div>
+  )
+}
+
+export default ProfileMenu
